2ba0fe646abee7c719fe4f5e5d1d73c79132160e
Geckuss <scrobalt@gmail.com>
2024-01-05 15:26:29 +0200

    V3 Blocks duplicates and auto-search

--- steam-profile-monitor-front/src/App.jsx
+++ steam-profile-monitor-front/src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import axios from 'axios';
 
 function App() {
   const [steamId, setSteamId] = useState('');
-  const [steamIds, setSteamIds] = useState([]);
+  const [steamIds, setSteamIds] = useState(() => {
+    const storedSteamIds = localStorage.getItem('steamIds');
+    return storedSteamIds ? JSON.parse(storedSteamIds) : [];
+  });
   const [profilesData, setProfilesData] = useState([]);
   const [error, setError] = useState(null);
 
@@ -12,36 +15,54 @@ function App() {
   };
 
   const isSteamIdValid = (steamId) => {
-    // Add your custom validation logic for Steam IDs
-    const steamIdRegex = /^\d{17}$/; // Steam IDs are 17 digits
+    const steamIdRegex = /^\d{17}$/;
     return steamIdRegex.test(steamId);
   };
 
   const handleAddProfile = () => {
     if (isSteamIdValid(steamId)) {
-      setSteamIds((prevIds) => [...prevIds, steamId]);
-      setSteamId('');
-      setError(null);
+      // Check if the Steam ID already exists in the list
+      if (steamIds.includes(steamId)) {
+        setError('Profile already added');
+      } else {
+        setSteamIds((prevIds) => {
+          const newIds = [...prevIds, steamId];
+          localStorage.setItem('steamIds', JSON.stringify(newIds));
+          return newIds;
+        });
+        setSteamId('');
+        setError(null);
+      }
     } else {
       setError('Invalid Steam ID format');
     }
   };
 
-  const handleSearch = async () => {
-    try {
-      const profilesResponses = await Promise.all(
-        steamIds.map(async (steamId) => await axios.get(`http://localhost:3000/profile/${steamId}`))
-      );
-      
-      const profilesData = profilesResponses.map((response) => response.data);
-      setProfilesData(profilesData);
-      setError(null);
-    } catch (error) {
-      setProfilesData([]);
-      setError('Error fetching profiles');
-    }
+  const handleClearProfiles = () => {
+    localStorage.removeItem('steamIds');
+    setSteamIds([]);
+    setProfilesData([]);
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const profilesResponses = await Promise.all(
+          steamIds.map(async (steamId) => await axios.get(`http://localhost:3000/profile/${steamId}`))
+        );
+  
+        const profilesData = profilesResponses.map((response) => response.data);
+        setProfilesData(profilesData);
+        setError(null);
+      } catch (error) {
+        setProfilesData([]);
+        setError('Error fetching profiles');
+      }
+    };
+  
+    fetchData();
+  }, [steamIds]); // This useEffect will re-run whenever steamIds change
+  
   return (
     <div>
       <h1>Steam Profile Monitor</h1>
@@ -54,7 +75,7 @@ function App() {
         />
       </label>
       <button onClick={handleAddProfile}>Add Profile</button>
-      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleClearProfiles}>Clear All Profiles</button>
 
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
@@ -63,14 +84,14 @@ function App() {
           <h2>Profiles Data</h2>
           {profilesData.map((profileData, index) => (
             <div key={index}>
-              <p>Username: {profileData.personaname}</p>
-              <p>Status: {profileData.onlineStatus}</p>
-              {profileData.onlineStatus === 'Online' && profileData.gamePlaying && (
+              <img src={profileData.avatar} alt={`Avatar for ${profileData.personaname}`} />
+              <p>{profileData.personaname}</p>
+              <p>{profileData.onlineStatus}</p>
+              {profileData.onlineStatus === 'Online' && profileData.gameId && (
                 <div>
                   <p>Playing: {profileData.gamePlaying}</p>
                 </div>
               )}
-              <img src={profileData.avatar} alt={`Avatar for ${profileData.personaname}`} />
             </div>
           ))}
         </div>
